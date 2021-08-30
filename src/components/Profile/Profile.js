0c9652222ab160fcb0bc4e4a6dd81d1d4ea25857
stas-yushkov <stas.yushkov@gmail.com>
2021-08-31 01:28:05 +0300

    add defaultProps & propTypes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -37,12 +37,20 @@ function Profile({
   );
 }
 
+Profile.defaultProps = {
+  avatar: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+};
+
 Profile.propTypes = {
-  name: PropTypes.string,
-  // tag,
-  // location,
-  // avatar,
-  // stats: { followers, views, likes },
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
 
 export default Profile;
