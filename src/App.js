7ba593cddf56041f8a81faa2480f4742fe44e292
stas-yushkov <stas.yushkov@gmail.com>
2021-09-02 01:30:11 +0300

    test App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Section from './components/Section';
 export default function App() {
   return (
     <>
+      <h1>Hello</h1>
+
       <Section title="Топ недели">
         <PaintingList items={paintings} />
       </Section>
