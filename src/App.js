80661f4b4ca0cd39476e7d9fe8371fb51dc5325c
stas-yushkov <stas.yushkov@gmail.com>
2021-09-02 00:18:43 +0300

    paintingList.map key={painting.title}

--- src/App.js
+++ src/App.js
@@ -4,30 +4,19 @@ import Painting from './components/Painting';
 export default function App() {
   return (
     <>
-      <Painting
-        imageUrl={paintingList[0].url}
-        title={paintingList[0].title}
-        authorName={paintingList[0].author.tag}
-        authorUrl={paintingList[0].author.url}
-        price={paintingList[0].price}
-        quantity={paintingList[0].quantity}
-      />
-      <Painting
-        imageUrl={paintingList[1].url}
-        title={paintingList[1].title}
-        authorName={paintingList[1].author.tag}
-        authorUrl={paintingList[1].author.url}
-        price={paintingList[1].price}
-        quantity={paintingList[1].quantity}
-      />
-      <Painting
-        imageUrl={paintingList[2].url}
-        title={paintingList[2].title}
-        authorName={paintingList[2].author.tag}
-        authorUrl={paintingList[2].author.url}
-        price={paintingList[2].price}
-        quantity={paintingList[2].quantity}
-      />
+      {paintingList.map(painting => {
+        return (
+          <Painting
+            key={painting.title}
+            imageUrl={painting.url}
+            title={painting.title}
+            authorName={painting.author.tag}
+            authorUrl={painting.author.url}
+            price={painting.price}
+            quantity={painting.quantity}
+          />
+        );
+      })}
     </>
   );
 }
