2a6908075f3f95e8d34f9a3a477c6329a30966ee
stas-yushkov <stas.yushkov@gmail.com>
2021-09-07 06:23:58 +0300

    npm install --save styled-components

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,9 +1,9 @@
 function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <li>
-      {avatar}
-      {name}
-      {isOnline}
+    <li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
     </li>
   );
 }
