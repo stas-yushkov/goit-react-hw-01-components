e03384c987f7fd0441b1561d08aff2c8b082ab38
stas-yushkov <stas.yushkov@gmail.com>
2021-09-13 09:55:36 +0300

    refacrot to FC

--- src/App.js
+++ src/App.js
@@ -2,31 +2,27 @@ import { name, tag, location, avatar, stats } from './data/user.json';
 import statisticalData from './data/statistical-data.json';
 import friends from './data/friends.json';
 
-import { Component } from 'react';
-
 import Profile from 'components/Profile';
 import Statistics from 'components/Statistics';
 import FriendList from 'components/FriendList';
 
 import './css/common.css';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <FriendList friends={friends} />
-        <Statistics title={'upload stats'} stats={statisticalData} />
-        <Statistics stats={statisticalData} />
-        <Profile
-          name={name}
-          tag={tag}
-          location={location}
-          avatar={avatar}
-          stats={stats}
-        />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <FriendList friends={friends} />
+      <Statistics title={'upload stats'} stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+    </>
+  );
 }
 
 export default App;
